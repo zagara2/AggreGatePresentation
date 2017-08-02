7ece1e969e73065a7cb8ae2dfe9ada59465b21a0
Mickey S <marimikaels@gmail.com>
2017-08-02 19:34:48 -0400

    initial

---
+++ client.js
@@ -0,0 +1,15 @@
+//clientside javascript
+
+
+// var ReactDOM = require('react-dom');
+
+// var routes = require('./routes/routes.jsx');
+
+
+
+// // Note how ReactDOM takes in two parameters (the contents and the location)
+// ReactDOM.render(
+// 	routes, document.getElementById("app")
+// );
+
+//you can have multiple ReactDOM.render statements
